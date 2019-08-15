58cf96e9558b63272cd7b834d81a9779901f7e9f
Jason Camacho <jasoncamacho@Jasons-MBP.attlocal.net>
2019-08-15 13:11:59 -0500

    MVP Completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,47 @@
-import React, { useState } from 'react';
+import React, {  Component } from 'react';
 import SavedList from './Movies/SavedList';
 import Movie from './Movies/Movie'
+import MovieList from "./Movies/MovieList";
+
 import {Route} from 'react-router-dom'
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      savedList: [],
+      movieInList: null
+    };
+  }
 
-const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+  addToSavedList = movie => {
+    const savedList = this.state.savedList;
+    const find = savedList.find(el => movie.id === el.id);
+    if (find) {
+      this.setState({ movieInList: `You've  saved  movie!` });
+      setTimeout(() => this.setState({ movieInList: null }), 2000);
+    } else {
+      savedList.push(movie);
+    }
 
-  const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
+    this.setState({ savedList });
   };
 
-  return (
-    <div>
-      <SavedList list={savedList} />
-      <Route exact path='\' components = {Movielist} />
-      <Route path ='/movies/:id' render = {props => (
-        <Movie {...props} addToSavedList= {this.addToSavedList} />
-      )} />
-    </div>
-  );
-};
-
-export default App;
+render() {
+    const { InList } = this.state;
+    return (
+      <div>
+        {InList !== null ? (
+          <h3 className="movie-warning">{InList}</h3>
+        ) : null}
+        <SavedList list={this.state.savedList} />
+        <Route exact path="/" component={MovieList} />
+        <Route
+          path="/movies/:id"
+          render={props => (
+            <Movie {...props} addToSavedList={this.addToSavedList} />
+          )}
+        />
+      </div>
+    );
+  }
+}
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,55 +1,41 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
+import MovieCard from './MovieCard';
 import {Link} from 'react-router-dom';
 
-const MovieList = props => {
-  const [movies, setMovies] = useState([])
-  useEffect(() => {
-    const getMovies = () => {
-      axios
-        .get('http://localhost:5000/api/movies')
-        .then(response => {
-          setMovies(response.data);
-        })
-        .catch(error => {
-          console.error('Server Error', error);
-        });
-    }
-    
-    getMovies();
-  }, []);
-  
-  return (
-    <div className="movie-list">
-      {movies.map(movie => (
-        <Link to ={`/mvoies/${movie.id}` }>
-          <MovieCard key={movie.id} movie={movie} />
-        </Link>
-      ))}
-    </div>
-  );
+export default class MovieList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/api/movies')
+      .then(response => {
+        this.setState(() => ({ movies: response.data }));
+      })
+      .catch(error => {
+        console.error('Server Error', error);
+      });
+  }
+
+  render() {
+    return (
+      <div className="movie-list">
+        {this.state.movies.map(movie => (
+          <MovieDetails key={movie.id} movie={movie} />
+        ))}
+      </div>
+    );
+  }
 }
 
 function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
+  const { id } = movie;
   return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div>
-  );
-}
-
-export default MovieList;
+    <Link to={`/movies/${id}`}>
+      <MovieCard movie={movie} />
+  </Link>)}
